098df4ff8b648a6dd6be03a79c2d3e63cd794879
Delsin <devagr@hotmail.com>
2021-04-19 20:02:59 +0200

    Get Request test

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const axios = require("axios").default;
 
 try {
+    const siteUrl = core.getInput("site");
 
+    axios
+    .get(siteUrl)
+    .then(function (response) {
+      let favicon = response.data
+      console.log(favicon);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 } catch (error) {
   core.setFailed(error.message);
 }
