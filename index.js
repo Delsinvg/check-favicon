062fa3612326a6d9c37da762c21fa89e729f8126
Delsin <devagr@hotmail.com>
2021-04-19 20:23:30 +0200

    Test

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ try {
       let favicon = response.data.match(
         /<link rel="icon" ([^<]+)>/
       );
-      console.log(favicon)
+      console.log(favicon[0])
     })
     .catch(function (error) {
       console.log(error);
