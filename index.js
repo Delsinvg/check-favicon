6fdc91fe19ac2936fb730ecedeae21babb29e28b
Delsin <devagr@hotmail.com>
2021-04-19 20:33:36 +0200

    Chaged +1 to +6

--- index.js
+++ index.js
@@ -31,9 +31,11 @@ try {
 
 function getHref(href) {
     console.log(href)
-    let start_pos = href.indexOf("href=\"") + 1;
+    let start_pos = href.indexOf("href=\"") + 6;
     console.log(start_pos);
     let end_pos = href.indexOf("\"", start_pos);
     console.log(end_pos);
     return resulthref = href.substring(start_pos, end_pos);
   }
+
+<link rel="icon" href="https://codedor.be/favicon.ico" type="image/x-icon"/>
