c2cf8091982c5e2b7aa5c4ef4d408e850b320292
Delsin <devagr@hotmail.com>
2021-04-20 19:57:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ try {
       }
 
       if (favicon) {
+        console.log(favicon[0])
         console.log(favicon[1])
         let faviconUrl = getHref(favicon[0])
         core.setOutput("favicon", faviconUrl);
